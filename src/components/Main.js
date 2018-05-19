306bfe46793f93f9e2945cabb1e96cf43b5d083c
Abel ko <esung1129@gmail.com>
2018-05-20 00:41:36 +0900

    node-env설정

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+
+    return (
+            <div className="main-template">
+                hello
+                <br/>
+                asdf
+            </div>
+    )
+}
+
+export default Main
