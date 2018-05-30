07c6f3692c8bd6b157ca5075305da5e929332fae
Abel ko <esung1129@gmail.com>
2018-05-30 22:55:58 +0900

    main page 완성, Link를 사용하여 button 누르면 페이지 전환

--- src/App/Reservation/pages/Main/index.js
+++ src/App/Reservation/pages/Main/index.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import classnames from "classnames/bind";
+import classnames from 'classnames/bind';
+import css from './index.scss';
 
-import css from "./index.scss";
+import { Link } from 'react-router-dom';
 
 const cx = classnames.bind(css);
-const moduleName = "Main";
+const moduleName = 'Main';
 
 const Main = () => {
   return (
     <div className={cx(`${moduleName}`)}>
       <div className={cx(`${moduleName}-Template`)}>
-        hello
-        <br />
-        asdf
+        <div className={cx(`${moduleName}-Buttons`)}>
+          <div className={cx(`${moduleName}-Buttons-Floor`)}>
+            <Link to="/first">1 FLOOR</Link>
+          </div>
+          <div className={cx(`${moduleName}-Buttons-Floor`)}>
+            <Link to="/third">3 FLOOR</Link>
+          </div>
+          <div className={cx(`${moduleName}-Buttons-Userlist`)}>
+            <Link to="/list">이용자 목록 보기</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
